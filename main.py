516901a10dbede599e2a8fd54cc4211f5332448c
i-am-lmi0 <arnav.somaghatta@icloud.com>
2025-06-18 12:58:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+# main.py
 
+import discord
+from discord.ext import commands
+import os
+from keep_alive import keep_alive
+
+TOKEN = os.environ["DISCORD_TOKEN"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong")
+
+# start web server
+keep_alive()
+
+# start bot
+bot.run(TOKEN)
