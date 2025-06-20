5d573c6eb9c2b7bc1581f7e8568819ebd5cf0582
i-am-lmi0 <arnav.somaghatta@icloud.com>
2025-06-20 11:56:43 -0400

    Update keep_alive.py

--- bot/keep_alive.py
+++ bot/keep_alive.py
@@ -1,17 +1,19 @@
 # keep_alive.py
 
 from flask import Flask
-from threading import Thread
+import threading
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "I'm alive!"
+def index():
+    return "DuckParadise bot is alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    thread = threading.Thread(target=run)
+    thread.start()
